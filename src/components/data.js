a37f19631269bc63d01119cbe1b809729cecb312
hanthienduc <hanthienduc.96@gmail.com>
2022-07-21 11:13:30 +0900

    install heroicons package, add details for projects, skills, and contact section

---
+++ src/components/data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "Quizzica App",
+    subtitle: "React and TypeScript",
+    link: "https://quizicca-app.vercel.app/",
+    image: "https://i.imgur.com/DbVmyFt.png",
+    description:
+      "A simple Quiz Application that get 5 random questions from Open Tria Via Database api",
+  },
+  {
+    title: "Quizzica App",
+    subtitle: "React and TypeScript",
+    link: "https://quizicca-app.vercel.app/",
+    image: "https://i.imgur.com/DbVmyFt.png",
+    description:
+      "A simple Quiz Application that get 5 random questions from Open Tria Via Database api",
+  },
+  {
+    title: "Quizzica App",
+    subtitle: "React and TypeScript",
+    link: "https://quizicca-app.vercel.app/",
+    image: "https://i.imgur.com/DbVmyFt.png",
+    description:
+      "A simple Quiz Application that get 5 random questions from Open Tria Via Database api",
+  },
+  {
+    title: "Quizzica App",
+    subtitle: "React and TypeScript",
+    link: "https://quizicca-app.vercel.app/",
+    image: "https://i.imgur.com/DbVmyFt.png",
+    description:
+      "A simple Quiz Application that get 5 random questions from Open Tria Via Database api",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Typescript",
+  "ReactJS",
+  "Redux",
+  "Nodejs",
+  "MongoDB",
+  "PostgreSQL",
+  "MySQL",
+  "Python",
+  "Django",
+];
