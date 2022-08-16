407ce499654ad1c4868a4c488846d9ae5d258f7d
hanthienduc <hanthienduc.96@gmail.com>
2022-08-16 14:46:52 +0900

    update github links

--- src/components/data.js
+++ src/components/data.js
@@ -2,21 +2,21 @@ export const projects = [
   {
     title: "My Library",
     subtitle: "MongoDB + React + TypeScript + NodeJS + Express",
-    link: "https://github.com/hanthienduc/ts-react-my-library-front",
+    link: "https://github.com/hanthienduc/react-my-library",
     image: "https://i.imgur.com/75fVVmq.png",
     description: "My Personal Library, Display all my favorite books and authors ",
   },
   {
     title: "Shopping Cart",
     subtitle: "ReactJS + TypeScript + ContextAPI",
-    link: "https://github.com/hanthienduc/ts-react-advanced-shopping-cart",
+    link: "https://github.com/hanthienduc/react-shopping-cart",
     image: "https://i.imgur.com/4aqTc97.png",
     description: "A Shopping Cart build with TypeScript, ReactJS, ContextAPI ",
   },
   {
     title: "Quizzical App",
     subtitle: "ReactJS + TypeScript",
-    link: "https://github.com/hanthienduc/quizicca-app",
+    link: "https://github.com/hanthienduc/react-quiziccal-app",
     image: "https://i.imgur.com/fQlercS.png",
     description:
       "A Quiz Application that gets 5 random questions from OTDB",
