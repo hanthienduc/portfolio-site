b0af9282376a7c378a991b5e6bf5925976b8d1e5
hanthienduc <hanthienduc.96@gmail.com>
2022-08-20 16:27:43 +0900

    blank element

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,11 @@ export default function Footer() {
     <>
       <div id='contact' className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row">
         <nav className="md:mr-auto md:ml-1 md:py-1 md:pl-1 md:border-1 md:border-gray-700 flex flex-wrap text-base justify-end">
-          <a href="https://www.linkedin.com/" className="flex items-center gap-1 mr-5 hover:text-white">
+          <a target={'_blank'}  href="https://www.linkedin.com/" className="flex items-center gap-1 mr-5 hover:text-white" rel="noreferrer">
             Linkedin <BsLinkedin />
           </a>
 
-          <a href="https://github.com/hanthienduc" className="flex items-center gap-1 mr-5 hover:text-white">
+          <a target={'_blank'} href="https://github.com/hanthienduc" className="flex items-center gap-1 mr-5 hover:text-white" rel="noreferrer">
             Github <BsGithub />
           </a>
 
