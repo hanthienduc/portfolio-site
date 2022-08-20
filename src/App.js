af9a182ed078a534895a867e095d52a7f344f664
hanthienduc <hanthienduc.96@gmail.com>
2022-08-20 16:13:17 +0900

    remove contact sessions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import About from "./components/About";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
