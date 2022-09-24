3ae927e2d4802fc601a3293bc6e5f1caff17e9d6
hanthienduc <hanthienduc.96@gmail.com>
2022-09-24 17:47:08 +0900

    update projects

--- src/components/data.js
+++ src/components/data.js
@@ -1,10 +1,18 @@
 export const projects = [
   {
-    title: "My Library",
-    subtitle: "React + TypeScript + ContextAPI",
-    link: "https://github.com/hanthienduc/react-my-library",
-    image: "https://i.imgur.com/75fVVmq.png",
-    description: "My Personal Library, Display all my favorite books and authors ",
+    title: "Netflix Clone",
+    subtitle: "NextJS + Firebase + TailwindCSS",
+    link: "https://github.com/hanthienduc/suntv",
+    image: "https://i.imgur.com/0yBdoQF.jpg",
+    description:
+      "A Netflix Clone application, discover popular Movies and TV Shows",
+  },
+  {
+    title: "Spotify Clone",
+    subtitle: "React + Redux + TailwindCSS",
+    link: "https://github.com/hanthienduc/lyriks",
+    image: "https://i.imgur.com/xFqFrd7.png",
+    description: "Music Application, Discover favorites songs and artists ",
   },
   {
     title: "Shopping Cart",
@@ -12,14 +20,6 @@ export const projects = [
     link: "https://github.com/hanthienduc/react-shopping-cart",
     image: "https://i.imgur.com/4aqTc97.png",
     description: "A Shopping Cart build with TypeScript, ReactJS, ContextAPI ",
-  },
-  {
-    title: "Netflix Clone",
-    subtitle: "NextJS + Firebase + TailwindCSS",
-    link: "https://github.com/hanthienduc/suntv",
-    image: "https://i.imgur.com/0yBdoQF.jpg",
-    description:
-      "A Netflix Clone application, discover popular Movies and TV Shows",
   },
   {
     title: "Reddit Comment",
